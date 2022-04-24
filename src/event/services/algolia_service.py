ed8ec9c624ff8737b1b46aca8a0536db6a3c287c
Aranxa Codina <a.codina@sfereno.com>
2022-04-24 14:50:56 +0200

    IN-76 : Fix after merge and update save_events-to_algolia to populate average mark

--- src/event/services/algolia_service.py
+++ src/event/services/algolia_service.py
@@ -6,7 +6,7 @@ from invisible_backend.settings import ALGOLIA_INDEX
 Sends events from the database to Algolia
 '''
 def save_events_to_algolia():
-    events = Event.objects.all()
+    events = Event.objects_with_mark
     events = ListEventSerializer.setup_for_serialization(events)
     data = ListEventSerializer(events, many=True).data
     ALGOLIA_INDEX.replace_all_objects(data)
